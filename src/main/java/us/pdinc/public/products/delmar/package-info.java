6b39677a23b9f769c152aee70b49801e50acade7
Jason Pyeron <jpyeron@pdinc.us>
2017-12-03 15:02:21 -0500

    license and description

---
+++ src/main/java/us/pdinc/public/products/delmar/package-info.java
@@ -0,0 +1,5 @@
+package us.pdinc.public.products.delmar;
+
+/**
+* Project Delmar is a database metadata tool for JDBC based dabases. It would be used in a Maven build to document and identify changes and relationships among database tables.
+*/
