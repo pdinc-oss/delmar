049541cbfcc7243a7b0a017cee4703b40c3ea090
Kyle Marek <kmarek@pdinc.us>
2017-12-04 01:42:32 -0500

    Rename us.pdinc.public to us.pdinc.oss
    "public" is not a valid package name.

--- src/main/java/us/pdinc/oss/products/delmar/package-info.java
+++ src/main/java/us/pdinc/oss/products/delmar/package-info.java
@@ -1,4 +1,4 @@
-package us.pdinc.public.products.delmar;
+package us.pdinc.oss.products.delmar;
 
 /**
 * Project Delmar is a database metadata tool for JDBC based dabases. It would be used in a Maven build to document and identify changes and relationships among database tables.
